0fbddc8e5fbe55228b5dd7cbc5ad0939d3f0477e
Jolanda <bregujola@gmail.com>
2022-10-28 11:36:26 +0200

    add firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,37 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyBSLSwGivLnMLHA3Atnbup5bRa4ULQ1dbo",
+  authDomain: "flight-ticket-scheduler-d75a1.firebaseapp.com",
+  projectId: "flight-ticket-scheduler-d75a1",
+  storageBucket: "flight-ticket-scheduler-d75a1.appspot.com",
+  messagingSenderId: "465624055780",
+  appId: "1:465624055780:web:1d62f50b1807a65ae0d1f1",
+  measurementId: "G-QEG5RYJDY9"
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSLSwGivLnMLHA3Atnbup5bRa4ULQ1dbo",
+  authDomain: "flight-ticket-scheduler-d75a1.firebaseapp.com",
+  projectId: "flight-ticket-scheduler-d75a1",
+  storageBucket: "flight-ticket-scheduler-d75a1.appspot.com",
+  messagingSenderId: "465624055780",
+  appId: "1:465624055780:web:1d62f50b1807a65ae0d1f1",
+  measurementId: "G-QEG5RYJDY9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
