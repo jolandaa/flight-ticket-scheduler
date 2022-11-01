373a8ac367c90a01e6534dfb64e771776fc9e48c
jolandaa <bregujola@gmail.com>
2022-11-01 19:52:34 +0100

    add dashboard chart

--- src/app/management/components/dashboard/dashboard.component.ts
+++ src/app/management/components/dashboard/dashboard.component.ts
@@ -13,10 +13,10 @@ export class DashboardComponent implements OnInit {
   public barChartPlugins = [];
 
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: [],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [], label: 'Inbound' },
+      { data: [], label: 'Outbound' },
     ]
   };
 
@@ -24,10 +24,26 @@ export class DashboardComponent implements OnInit {
     responsive: false,
   };
 
-  constructor(public ticketService: TicketService) {
-    this.ticketService.flightTicketList.subscribe(res => {
-      if (res) {
+  dataArr: any = [];
 
+  constructor(public ticketService: TicketService) {
+    this.ticketService.flightTicketList.subscribe(ticketArr => {
+      if (ticketArr) {
+        this.dataArr = ticketArr;
+        ticketArr.forEach(ticketEl => {
+          if (this.barChartData.labels?.indexOf(ticketEl.inbound) === -1) {
+            this.barChartData.labels?.push(ticketEl.inbound)
+          }
+          if (this.barChartData.labels?.indexOf(ticketEl.outbound) === -1) {
+            this.barChartData.labels?.push(ticketEl.outbound)
+          }
+        })
+        this.barChartData.labels?.forEach(ticket => {
+          const inboundFilterValue = ticketArr.filter(el => el.inbound === ticket).length;
+          this.barChartData.datasets[0].data.push(inboundFilterValue)
+          const outboundFilterValue = ticketArr.filter(el => el.outbound === ticket).length;
+          this.barChartData.datasets[1].data.push(outboundFilterValue)
+        })
       }
     })
   }
